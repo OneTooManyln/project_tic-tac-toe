b9ee9c8c27971085ff2578782c893023e6055233
OneTooMany <santosismael.115@gmail.com>
2023-04-23 17:14:07 -0700

    Create emtpy array of 3 to popluate grid
    Loop through array to add class of grids to each item, and
    display on html.

--- ttt.js
+++ ttt.js
@@ -1,5 +1,18 @@
-const gameBoard = (() => {
-  const board = new Array(3);
-})();
+const gameBoard = () => {
+  const board = [];
+
+  for (i = 0; i < 3; i++) {
+    board.push("");
+  }
+
+  const gridContainer = document.querySelector(".grid-container");
+  board.forEach((item, index) => {
+    const grids = document.createElement("div");
+    grids.classList.add("grids");
+    gridContainer.appendChild(grids);
+  });
+  return board;
+};
+gameBoard();
 
 const createPlayer = (name, marker) => ({ name, marker });
