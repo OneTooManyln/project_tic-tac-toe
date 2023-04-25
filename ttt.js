df0604f4f2afcb5f58e889159cf036ce7a52911c
OneTooMany <santosismael.115@gmail.com>
2023-04-25 11:28:18 -0700

    Assign innertext for each grid created to equal array value

--- ttt.js
+++ ttt.js
@@ -1,14 +1,16 @@
 const gameBoard = (() => {
-  const board = ["X", "X", "O"];
+  const board = ["X", "X", "O", "O", "X", "0", "0", "0", "X"];
 
   /*   for (i = 0; i < 3; i++) {
     board.push("");
   } */
 
   const gridContainer = document.querySelector(".grid-container");
-  board.forEach(() => {
+  board.forEach((item, index) => {
     const grids = document.createElement("div");
     grids.classList.add("grids");
+    grids.innerText = `${item}`;
+    console.log(`${item} ${index}`);
     gridContainer.appendChild(grids);
   });
   console.table(board);
